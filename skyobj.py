e059d8a416f2f4ebd957595af7f46709c6b8dc89
Peter McGill <petermcgill@Peters-MacBook-Pro.local>
2017-12-05 18:13:48 +0000

    initialise with some constructor tests

--- skyobj.py
+++ skyobj.py
@@ -1,13 +1,27 @@
 
 
 
-
-
-
 class skyobj(object):
 
 
-	def _init_(self,id,ra,dec,epoch,pmra,pmdec):
+	def __init__(self,id=None,ra=None,dec=None,epoch=None,pmra=None,pmdec=None,parallax=None):
+		"""
 
+		
+
+		"""
+		
+		
+		self.id  = id
+		self.ra = ra
+		self.dec = dec
+		self.epoch = epoch
+		
+		#set proper motions and parallax to zero if
+		#not specified.
+		self.pmra = pmra if pmra else 0.0
+		self.pmdec = pmdec if pmdec else 0.0
+		self.parallax = parallax if parallax else 0.0
+		
 
 
--- test_skyobj.py
+++ test_skyobj.py
@@ -1,4 +1,50 @@
-import unittest 
+import sys
+from skyobj import skyobj
+import unittest
+
+class Testskyobj(unittest.TestCase):
+	
+	
+	def setUp(self):
+		self.obj1 = skyobj(123456789,30.0,60.0,2015.0,100.0,100.0,200.0)
+		self.obj2 = skyobj(123456789,30.0,60.0,2015.0)
+	
+	def test_id_init(self):
+		self.assertEqual(self.obj1.id,123456789)
+
+	def test_ra_init(self):
+		self.assertEqual(self.obj1.ra,30.0)
+
+	def test_dec_init(self):
+		self.assertEqual(self.obj1.dec,60.0)
+
+	def test_epoch_init(self):
+		self.assertEqual(self.obj1.epoch,2015.0)
+
+	def test_pmra_init(self):
+		self.assertEqual(self.obj1.pmra,100.0)
+	
+	def test_pmdec_init(self):
+		self.assertEqual(self.obj1.pmdec,100.0)
+
+	def test_parallax_init(self):
+		self.assertEqual(self.obj1.parallax,200.0)
+
+	def test_pmra_none_init(self):
+		self.assertEqual(self.obj2.pmra,0.0)
+	
+	def test_pmdec_none_init(self):
+                self.assertEqual(self.obj2.pmdec,0.0)
+
+	def test_parallax_none_init(self):
+                self.assertEqual(self.obj2.parallax,0.0)
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+		
+	 
 
 
 
