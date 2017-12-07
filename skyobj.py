e41699047eec1307ff4bedf8baa830b3536dee6b
Peter McGill <petermcgill@Peters-MacBook-Pro.local>
2017-12-07 08:28:59 +0000

    tagnet plane coordinate function

--- skyobj.py
+++ skyobj.py
@@ -22,6 +22,9 @@ class skyobj(object):
 		self.dec = dec
 		self.epoch = epoch
 		
+		self.xi,self.eta = self.s2tp(ra,dec)
+	
+				
 		#set proper motions and parallax to zero if
 		#not specified.
 		self.pmra = pmra if pmra else 0.0
@@ -30,3 +33,38 @@ class skyobj(object):
 		
 
 
+
+	def s2tp(ra, dec):
+    		"""
+		Convert from spherical coordinate system
+		to tagent plane coordinates
+		"""
+ 
+		#Set the position of the tagent plane
+                #at the position of the lens
+		raz = self.ra
+		decz = self.dec
+ 
+		# convert to radians
+		_rar = np.radians(ra)
+		_razr = np.radians(raz)
+		_decr = np.radians(dec)
+    		_deczr = np.radians(decz)
+ 
+		# Trig functions
+		_sdecz = np.sin(_deczr)
+		_sdecj = np.sin(_decr)
+		_cdecz = np.cos(_deczr)
+		_cdecj = np.cos(_decr)
+		_radifj = _rar - _razr
+		_sradifj = np.sin(_radifj)
+		_cradifj = np.cos(_radifj)
+ 
+		# Reciprocal of star vector length to tangent plane
+    		_denomj = (_sdecj*_sdecz)+(_cdecj*_cdecz*_cradifj)
+ 
+		# Compute tangent plane coordinates
+		xi = ((180/np.pi)*3600)*_cdecj*_sradifj/_denomj
+		eta = ((180/np.pi)*3600)*(_sdecj*_cdecz-_cdecj*_sdecz*_cradifj)/_denomj
+ 
+		return xi, eta
